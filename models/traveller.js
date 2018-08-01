d1f2f60158307b9057229d7f62c4f0b0ba6918ab
jamos90 <jamos_98@hotmail.com>
2018-08-01 15:23:56 +0100

    first function created and test passed

---
+++ models/traveller.js
@@ -0,0 +1,36 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.location)
+
+
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journeys.endLocation)
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
